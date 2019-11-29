4ef1031370b2c221bf276a007d5cf25a728cdbc2
Juan Alvarez Duque <jalvarez@tp-1a207-35.enst.fr>
2019-11-29 15:24:33 +0100

    almost

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+	static final String DEFAULT_HOST = "localhost";
+	static final int DEFAULT_PORT = 3331;
 	public static void main(String[] args) {
-		MyFrame myFrame = new MyFrame();
+		Client client = null;
+		try {
+			client = new Client(DEFAULT_HOST,DEFAULT_PORT);
+		}
+		catch(Exception e){
+			System.err.println("Client: Couldn't connect to "+DEFAULT_HOST+":"+DEFAULT_PORT);
+			System.exit(1);
+	    }
+		System.out.println("Client connected to "+DEFAULT_HOST+":"+DEFAULT_PORT);
+		MyFrame myFrame = new MyFrame(client);
+		
 	}
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -11,15 +11,15 @@ import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 
 public class MyFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
-	
-	public MyFrame() {
-
-		System.out.println("hi");
-		
+	private Client client;
+	private AbstractAction actionFind, actionPlay, actionClose;
+	public MyFrame(Client client) {
+		this.client = client;
 		JTextArea textArea = new JTextArea();
 
         JScrollPane jp = new JScrollPane(textArea);
@@ -27,42 +27,43 @@ public class MyFrame extends JFrame {
 		
 		JPanel southFrame = new JPanel();
 		
-		AbstractAction action = new AbstractAction("Button 1") {
+		JTextField targetField = new JTextField("Target media",10);
+		southFrame.add(targetField,BorderLayout.SOUTH);
+		
+		actionFind = new AbstractAction("Find") {
+			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				textArea.append(e.getSource().toString());
+				String targetMedia = targetField.getText();
+				String response = client.send("find " + targetMedia);
+				textArea.append(response);
 			}
 		};
-		
-		JButton b1 = new JButton();
-		b1.addActionListener(new ActionListener() {
-			
+		actionPlay = new AbstractAction("Play") {
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				textArea.append("Button 1! \n");
-				
+				textArea.append(this.getValue(NAME).toString()+"! \n");
 			}
-		});
-		southFrame.add(b1);
-		JButton b2 = new JButton();
-		b2.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				textArea.append("Button 2! \n");
-				
-			}
-		});
-		southFrame.add(b2);
-		JButton b3 = new JButton();
-		b3.addActionListener(new ActionListener() {
-			
+		};
+		actionClose = new AbstractAction("Close") {
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
-		});
+		};
+		
+		JButton b1 = new JButton(actionFind);
+		southFrame.add(b1);
+		
+		JButton b2 = new JButton(actionPlay);
+		southFrame.add(b2);
+		
+		JButton b3 = new JButton(actionClose);
 		southFrame.add(b3);
 		
 		this.add(southFrame,BorderLayout.SOUTH);
@@ -70,16 +71,19 @@ public class MyFrame extends JFrame {
 		
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("Menu");
-		menu.add(action);
+		menu.add(actionFind);
+		menu.add(actionPlay);
 		menuBar.add(menu);
 		this.setJMenuBar(menuBar);
 		
 		JToolBar toolBar = new JToolBar("Toolbar");
+		toolBar.add(actionFind);
+		toolBar.add(actionPlay);
 		this.add(toolBar, BorderLayout.NORTH);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setPreferredSize(new Dimension(400, 300));
-		this.setTitle("JSwing!");
+		this.setPreferredSize(new Dimension(400, 400));
+		this.setTitle("Telecommande Swing!");
 		this.pack();
 		this.setVisible(true);
 	}
